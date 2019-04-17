07397f8677655f3f0714d2a32527ce3bb0a8b5d0
ARusani <alessandro.rusani@gmail.com>
2019-04-17 03:17:38 +0200

    Web page with async call

--- App/js/index.js
+++ App/js/index.js
@@ -1,7 +1,7 @@
+/* eslint-disable max-len */
 /* eslint-disable one-var */
 /* eslint-disable require-jsdoc */
 
-//window.onload = async () => {
 window.addEventListener('load', async () => {
   if (typeof web3 !== 'undefined') {
     window.web3 = new Web3(web3.currentProvider);
@@ -9,17 +9,51 @@ window.addEventListener('load', async () => {
     window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:9545'));
   }
 
-  console.log(window.web3.version.api)
-  let alice, bob, carol; 
-  const accounts = window.web3.eth.accounts[0]
-  console.log('Attempting to deploy from account', accounts);
-  
-  const balance = await web3.eth.getBalance(accounts[0]);
-  console.log("balance", web3.utils.fromWei(balance, "ether"));
+  console.log(window.web3.version.api);
 
+  const accounts = await window.web3.eth.accounts;
+  const alice = accounts[0];
+  const bob = accounts[1];
+  const carol = accounts[2];
+  console.log('Attempting to deploy from account', accounts);
 
 
   const SplitterContract = web3.eth.contract([
+    {
+      'constant': false,
+      'inputs': [],
+      'name': 'stop',
+      'outputs': [],
+      'payable': false,
+      'stateMutability': 'nonpayable',
+      'type': 'function',
+      'signature': '0x07da68f5'
+    },
+    {
+      'constant': true,
+      'inputs': [],
+      'name': 'isStopped',
+      'outputs': [
+        {
+          'name': '',
+          'type': 'bool'
+        }
+      ],
+      'payable': false,
+      'stateMutability': 'view',
+      'type': 'function',
+      'signature': '0x3f683b6a'
+    },
+    {
+      'constant': false,
+      'inputs': [],
+      'name': 'unStop',
+      'outputs': [],
+      'payable': false,
+      'stateMutability': 'nonpayable',
+      'type': 'function',
+      'signature': '0x52486654'
+    },
     {
       'constant': false,
       'inputs': [],
@@ -27,7 +61,8 @@ window.addEventListener('load', async () => {
       'outputs': [],
       'payable': false,
       'stateMutability': 'nonpayable',
-      'type': 'function'
+      'type': 'function',
+      'signature': '0x715018a6'
     },
     {
       'constant': true,
@@ -41,7 +76,8 @@ window.addEventListener('load', async () => {
       ],
       'payable': false,
       'stateMutability': 'view',
-      'type': 'function'
+      'type': 'function',
+      'signature': '0x8da5cb5b'
     },
     {
       'constant': true,
@@ -55,7 +91,8 @@ window.addEventListener('load', async () => {
       ],
       'payable': false,
       'stateMutability': 'view',
-      'type': 'function'
+      'type': 'function',
+      'signature': '0x8f32d59b'
     },
     {
       'constant': true,
@@ -74,7 +111,8 @@ window.addEventListener('load', async () => {
       ],
       'payable': false,
       'stateMutability': 'view',
-      'type': 'function'
+      'type': 'function',
+      'signature': '0xd5d44d80'
     },
     {
       'constant': false,
@@ -88,13 +126,15 @@ window.addEventListener('load', async () => {
       'outputs': [],
       'payable': false,
       'stateMutability': 'nonpayable',
-      'type': 'function'
+      'type': 'function',
+      'signature': '0xf2fde38b'
     },
     {
       'inputs': [],
       'payable': false,
       'stateMutability': 'nonpayable',
-      'type': 'constructor'
+      'type': 'constructor',
+      'signature': 'constructor'
     },
     {
       'anonymous': false,
@@ -106,7 +146,8 @@ window.addEventListener('load', async () => {
         }
       ],
       'name': 'EventSplitterCreated',
-      'type': 'event'
+      'type': 'event',
+      'signature': '0xeaa38c1da9a5a7804ce694794a8da099db876d00d8ff591d5236a6421c9330f9'
     },
     {
       'anonymous': false,
@@ -138,7 +179,8 @@ window.addEventListener('load', async () => {
         }
       ],
       'name': 'EventEtherSplitted',
-      'type': 'event'
+      'type': 'event',
+      'signature': '0x2f86fdf32f263334aba74ce1634cdf359d907714e308d2af65ae5ad3614e4544'
     },
     {
       'anonymous': false,
@@ -155,7 +197,34 @@ window.addEventListener('load', async () => {
         }
       ],
       'name': 'EventWithdraw',
-      'type': 'event'
+      'type': 'event',
+      'signature': '0x41caea7d684d5ba5fba1924d54c5169a147971fb5ffdde987624e8bbd450828b'
+    },
+    {
+      'anonymous': false,
+      'inputs': [
+        {
+          'indexed': true,
+          'name': 'caller',
+          'type': 'address'
+        }
+      ],
+      'name': 'EventStopped',
+      'type': 'event',
+      'signature': '0x431c6f6c58ff41e8cee840a02edad857e13381cd6b0772cc84dc0edfcc683592'
+    },
+    {
+      'anonymous': false,
+      'inputs': [
+        {
+          'indexed': true,
+          'name': 'caller',
+          'type': 'address'
+        }
+      ],
+      'name': 'EventUnStopped',
+      'type': 'event',
+      'signature': '0x3f11058ac63f39665f9d62070804230a03741b733b8c4d0c6f6e069949dda8b6'
     },
     {
       'anonymous': false,
@@ -172,7 +241,8 @@ window.addEventListener('load', async () => {
         }
       ],
       'name': 'OwnershipTransferred',
-      'type': 'event'
+      'type': 'event',
+      'signature': '0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0'
     },
     {
       'constant': false,
@@ -190,7 +260,8 @@ window.addEventListener('load', async () => {
       'outputs': [],
       'payable': true,
       'stateMutability': 'payable',
-      'type': 'function'
+      'type': 'function',
+      'signature': '0xcd04ab34'
     },
     {
       'constant': false,
@@ -199,91 +270,98 @@ window.addEventListener('load', async () => {
       'outputs': [],
       'payable': false,
       'stateMutability': 'nonpayable',
-      'type': 'function'
+      'type': 'function',
+      'signature': '0x3ccfd60b'
     }
   ]);
-  const SplitterAddress = '0xBaAbdBB6b41d3dC9AE18AE361452F9eedf90d679';
+  const SplitterAddress = '0xdf649A4f4D601eBF4D817B9E9dcaE02CD60eb89f';
   const SplitterInstance = SplitterContract.at(SplitterAddress);
 
-  refreshBalance();
+  await refreshBalance();
 
-  $('#splitEthers').click(function() {
+  $('#splitEthers').click( async () => {
     $('#loader').show();
-    SplitterInstance.splitEthers(bob, carol, {from: alice, value: $('#Ethers').val()}, function(err, res) {
-      if (err) {
-        $('loader').hide();
-        console.log(err);
-      }
-    });
-  });
-
-  SplitterInstance.EventEtherSplitted({}, 'latest').watch(function(error, result) {
-    if (!error) {
+    try {
+      const result = await SplitterInstance.splitEthers(bob, carol, {from: alice, value: $('#Ethers').val()});
+      console.log(result.toString());
       $('#loader').hide();
-
-      refreshBalance();
-    } else {
+    } catch (error) {
       $('#loader').hide();
-    }
-    console.error(error);
+      $('#contractBalance').html(error.toString());
+    };
   });
 
-  $('#withdrawBob').click(function() {
-    $('#loader').show();
-    SplitterInstance.withdraw({from: bob}, function(err) {
-      if (err) {
-        $('loader').hide();
-        console.log(err);
-      }
-    });
-  });
-
-  $('#withdrawCarol').click(function() {
-    $('#loader').show();
-    SplitterInstance.withdraw({from: carol}, function(err, res) {
-      if (err) {
-        $('loader').hide();
-        console.log(err);
-      }
-    });
-  });
-
-  SplitterInstance.EventWithdraw({}, 'latest').watch(function(error, result) {
-    if (!error) {
-      $('#loader').hide();
-
-      refreshBalance();
-    } else {
-      $('#loader').hide();
-    }
-    console.error(error);
-  });
-
-  function refreshBalance() {
-    web3.eth.getBalance( SplitterAddress, function(error, result) {
+  try {
+    SplitterInstance.EventEtherSplitted({}, 'latest').watch((error, log) => {
       if (!error) {
-        $('#contractBalance').html(result.toString() + ' Total Eheters in the contract');
-
-        console.log(result.toString());
+        console.log('Watched Log:', log.args);
+        refreshBalance();
       } else {
-        console.error(error);
+        $('#loader').hide();
+        console.log('Watched Error:', error);
       }
     });
+
+    SplitterInstance.EventWithdraw({}, 'latest').watch((error, log) => {
+      if (!error) {
+        console.log('Watched Log:', log.args);
+        refreshBalance();
+      } else {
+        $('#loader').hide();
+        console.log('Watched Error:', error);
+      }  
+    });
+  } catch (error) {
+    $('#loader').hide();
+    $('#contractBalance').html(error.toString());
+  };
+
+
+  $('#withdrawBob').click( async () => {
+    $('#loader').show();
+    await clickButton(bob);
+  });
+
+  $('#withdrawCarol').click(async () => {
+    $('#loader').show();
+    await clickButton(carol);
+  });
+
+  async function clickButton(anAddress) {
+    try {
+      const result = await SplitterInstance.withdraw({from: anAddress});
+      console.log(result);
+      $('#loader').hide();
+    } catch (error) {
+      $('#loader').hide();
+      $('#contractBalance').html(error.toString());
+    };
+  };
+
+  async function refreshBalance() {
+    try {
+      const result = await web3.eth.getBalance( SplitterAddress);
+      console.log('Balance Result: ',result.toString());
+      $('#contractBalance').html(result.toString() + ' Total Ethers in the contract');
+    } catch (error) {
+      $('#loader').hide();
+      $('#contractBalance').html(error.toString());
+    };
 
     const addesses = [
       {'address': bob, 'tag': '#bobBalance'},
       {'address': carol, 'tag': '#carolBalance'},
     ];
 
-    addesses.forEach(function(anAddress) {
-      SplitterInstance.credit.call(anAddress.address, function(error, result) {
-        if (!error) {
-          $(anAddress.tag).html(result.toString());
-          console.log(result.toString());
-        } else {
-          console.error(error);
-        }
-      });
+    addesses.forEach(async (anAddress) => {
+      try {
+        const result = await SplitterInstance.credit(anAddress.address);
+        console.log(anAddress.address + ' = ' +result.toString());
+        $(anAddress.tag).html(result.toString());
+      } catch (error) {
+        $('#loader').hide();
+        $('#contractBalance').html(error.toString());
+      };
     });
   };
 });
